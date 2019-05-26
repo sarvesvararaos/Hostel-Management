5c1cb9891ba62501f6cd0338862d64be839bdd7e
Sarvesh <Sarvesh@192.168.0.7>
2019-05-26 22:50:30 +0530

    commited

--- src/main/java/main/Hostel.java
+++ src/main/java/main/Hostel.java
@@ -34,24 +34,30 @@ public class Hostel{
 		   try{
 	             BufferedReader br = new BufferedReader(new FileReader("hostel.txt"));
 	               String line = null;
-	            
-	               //write your code here !!!
 	               while ((line = br.readLine()) != null) {
 		               String[] splited = line.split("\\s+");
 		               String checkName = splited[0];
-		               //write your code here !!!
-//		               compare check name with name and return true if present and false if not
-	               }
+		               if(splited[0].equals(name))
+		               {
+		            	   return true;//write your code here !!!
+		               }
+		               }
 	               
 	               
 	            }catch(Exception e){
 	                System.out.println(e);
 	            }
-			return true;
+			return false;
 	   }
        public static void allotHostel(){
     	   //write your code here!!!
-    	   
+    	   try {
+    		   writedata();
+    		   
+    	   }catch(IOException e)
+    	   {
+    		   System.out.println(e);
+    	   }
        }
 
        public static boolean verifyStudent(int regNo){
@@ -76,7 +82,7 @@ public class Hostel{
     	   boolean chk = true;
     	   
     	   //write your code here
-    	   
+    	   chk=readData(name);
     	   return chk;
         }
         
